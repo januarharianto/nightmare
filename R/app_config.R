1c27733fb3178757171e0c2d129d8db08ddde1a4
Januar Harianto <jhar8696@sydney.edu.au>
2026-03-27 12:06:52 +1100

    Bump version to 0.2.1

--- R/app_config.R
+++ R/app_config.R
@@ -6,7 +6,7 @@
 
 NIGHTMARE_CONFIG <- list(
   ui = list(
-    version = "0.2.0",
+    version = "0.2.1",
     app_name = "NIGHTMARE",
     subtitle = "New Incredibly Glitchy Hacking Tool to Manage All Records Effortlessly",
     author = "Januar Harianto"
